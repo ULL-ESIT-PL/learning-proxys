8227c20aa63cd85aed374c13f456d6dde158f2a8
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-03-31 21:34:03 +0100

    neutral.js

---
+++ neutral.js
@@ -0,0 +1,48 @@
+
+// a neutral element for all oeprations
+
+const Neutral = function (x) { 
+    return x; 
+};
+Neutral.toString = function() { return ""; }
+
+//Neutral.toString = function() { return ""; }
+
+class FallbackHandler {
+    constructor(target) {
+        this.target = target;
+    }
+
+    get(target, prop, receiver) {
+        console.log("prop=",prop, "type=", typeof target[prop]) ;
+        if (typeof target[prop] === 'function') {
+            return target[prop].bind(target); // Ensure methods are bound to the target object
+        } else if (typeof target[prop] === 'undefined') {
+            return (x) => x; // Implement fallback behavior here
+        } else {
+            return target[prop];
+        }
+    }
+}
+
+const neutral = new Proxy(Neutral, new FallbackHandler());
+String.prototype.add = function (x) { 
+    console.log(x === neutral);
+    if (x == neutral) return this.toString();
+    return this+x;
+}
+
+Number.prototype.add = function (x) { 
+    if (x == neutral) return Number(this);
+    return this+x;
+}
+// Test method calls
+console.error("hello".add(neutral)); // Output: hello
+console.error(neutral.add("world")); // Output: world
+console.error("neutral length = ",neutral.length); // 1
+console.log(neutral.add(4)); // Output: 4
+console.log("4.add(neutral) = ",(4).add(neutral)); // Output: 4
+
+console.error(neutral.mul(4)); // Output: 4
+console.error(neutral(5)); // Output: 5
+//console.log(yProxy.fallback('Fallback executed')); // Output: Fallback executed
