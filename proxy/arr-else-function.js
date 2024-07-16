9b7b354dd1a156a68621da3476df302af51b6c7e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-07-16 11:33:30 +0100

    proxy allows to implement objects and array with default behaviors

---
+++ proxy/arr-else-function.js
@@ -0,0 +1,23 @@
+// https://stackoverflow.com/questions/6600868/set-default-value-of-javascript-object-attributes
+/* Consider the syntax 
+arr = [3, 2, 1] else x => x*x;
+*/
+let arr = [3, 2, 1];
+
+let handler2 = {
+  get: function (target, name) {
+    if (target.hasOwnProperty(name))
+      return target[name];
+    return Number(name)*Number(name);
+  }
+};
+
+let s = new Proxy(arr, handler2);
+try {
+  let r = s[9]; // => 81
+  console.log(r);
+  s[9] = 9;
+  console.log(s[9]); // => 9
+} catch (e) {
+  console.log(e.message);
+}
