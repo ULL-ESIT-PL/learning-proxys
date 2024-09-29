9011367404141ab702fb8bdb1b536eb9d2048b5e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-09-29 12:50:57 +0100

    callable-objects

--- in-place/modifiable-functions-via-callable-objects.js
+++ in-place/modifiable-functions-via-callable-objects.js
@@ -12,18 +12,16 @@ class CachedFunction extends Callable {
         super()
         this.function = f;
         this.cache = new Map();
-        console.log(this.cache)
     }
 
     _call(arg) {
-        let result = this.cache.get(arg);
-        return result || this.function(arg);
+        return this.cache.get(arg) || this.function(arg);
     }
 
 }
 
 let cf = new CachedFunction(x => x*2);
 
-console.log(cf(1)); // 2
-cf.cache.set(1, -1);
-console.log(cf(1)); // -1
+console.log(cf(1));  // 2
+cf.cache.set(1, -1); // Low level version of cf(1) = -1
+console.log(cf(1));  // -1
