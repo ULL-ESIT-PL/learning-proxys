a0ee74e78a5b2c3bc94e620e40fc534c40c831c8
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-10-21 09:46:56 +0100

    reviwed by boriel

--- in-place/modifiable-functions-via-callable-objects.js
+++ in-place/modifiable-functions-via-callable-objects.js
@@ -26,3 +26,6 @@ let cf = new CachedFunction(x => x*2);
 console.log(cf(1));  // 2
 cf.cache.set(1, -1); // Low level version of cf(1) = -1
 console.log(cf(1));  // -1
+console.log(cf(0));  // 0
+cf.cache.set(0, -2); // Low level version of cf(0) = -2
+console.log(cf(0));  // -2
